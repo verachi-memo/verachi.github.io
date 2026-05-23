e9faff2285eda468b3fe32a8725d006b7c57c8ca
Memo Garcia <memogarcia@protonmail.ch>
2026-05-23 12:27:05 +0900

    Harden static site CSP

---
+++ home/analytics.js
@@ -0,0 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+window.gtag = gtag;
+gtag('js', new Date());
+gtag('config', 'G-NHXTJ2NZMT');
