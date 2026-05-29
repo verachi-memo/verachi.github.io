2265be3c9b18e048834813e7e358a5c5a63f9231
Memo Garcia <memogarcia@protonmail.ch>
2026-05-29 14:59:16 +0900

    performance

--- home/analytics.js
+++
@@ -1,9 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-
-window.gtag = gtag;
-gtag('js', new Date());
-gtag('config', 'G-NHXTJ2NZMT');
